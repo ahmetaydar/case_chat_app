d9c3aff8fa1d2d7c6c740f54bd7c1a573338713c
ahmetaydar <a.aydar2014@gmail.com>
2023-07-13 02:43:36 +0300

    added messages comp

---
+++ src/components/MessageForm.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+const MessageForm = () => {
+  const [value, setValue] = useState("");
+  const handleSubmit = () => {};
+
+  const handleChange = () => {};
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message"
+        value={value}
+        onChange={handleChange}
+      />
+    </form>
+  );
+};
+
+export default MessageForm;
